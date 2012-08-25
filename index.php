6e96d8343f3a42a826a6a225da1a94398e0f6484
Robert Doucette <rice8204@gmail.com>
2012-08-25 21:09:07 +0200

    updating CodeIgniter and making sure the sessions table is always available

--- index.php
+++ index.php
@@ -2,6 +2,26 @@
 // uncomment if the project will be using the french language
  setlocale(LC_TIME, 'fra', 'fr_FR', 'fr_FR.utf8');
 
+$mysqli = new mysqli('localhost', 'ibbl', 'password');
+if($mysqli->query("SHOW DATABASES LIKE 'ibbl'")->num_rows === 0)
+{
+	$mysqli->query("CREATE DATABASE `ibbl`");
+}
+$mysqli->select_db('ibbl');
+if($mysqli->query("SHOW TABLES LIKE 'ci_sessions'")->num_rows === 0)
+{
+	$mysqli->query("CREATE TABLE `ci_sessions` (
+			`session_id` varchar(40) NOT NULL default '0',
+			`ip_address` varchar(16) NOT NULL default '0',
+			`user_agent` varchar(120) NOT NULL,
+			`last_activity` int(10) unsigned NOT NULL default '0',
+			`user_data` text,
+			PRIMARY KEY  (`session_id`)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+	);
+}
+$mysqli->close();
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
